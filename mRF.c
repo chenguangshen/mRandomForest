eff606ff477cf1b6685365a4c2c1a78e658c537d
Chenguang Shen <pscggeforce@gmail.com>
2013-11-16 01:49:54 -0800

    finish mRF.c, fix error, add code comments

--- mRF.c
+++ mRF.c
@@ -7,119 +7,124 @@
 #define NUM_TREE 32
 #define NUM_NODE 550
 #define NUM_FEATURE 16
+#define DEBUG 0
 
 typedef unsigned int uint32;
 typedef unsigned short uint16;
 typedef unsigned char uchar;
-typedef unsigned char byte;
 typedef enum {EMPTY, BUILD_ME, TERMINAL, SPLIT} NodeStatusType;
 
+// basic element: tree node
 typedef struct tree_node_ {
   NodeStatusType status;
   uchar label;
   uint16 attr;
-  //uint16 start;
-  //uint16 size;
+  // store the index (in array) of childen nodes
   uint16 left;
   uint16 right;
-  //float entropy;
   float split_point;
-  //uchar depth;
 } tree_node;
 
+// tree in the forest
+// contains an array of treenode
 typedef struct tree_ {
-	short num_nodes;
+	uint16 num_nodes;
 	tree_node nodes[NUM_NODE];
 } tree;
 
-static short num_tree = 32;
-static short num_feature = 6;
+static uint16 num_tree = 32;
+static uint16 num_feature = 6;
 static tree forest[NUM_TREE];
 static char spacer[10];
-static short status_int;
-static short cur_node;
-static short label_int;
-int i;
+static uint16 status_int;
+static uint16 cur_node;
+static uint16 label_int;
+uint32 i, j, k;
 
-tree read_tree(FILE *fin) {
-	tree cur_tree;
-	tree_node cur;
-  fscanf(fin, "%s %d\n", spacer, &cur_tree.num_nodes);
-  //printf("from read_tree: num_node=%d\n", cur_tree.num_nodes);
-  //memset(cur_tree.nodes, 0, cur_tree.num_nodes * sizeof(tree_node));
-  for (i = 0; i < cur_tree.num_nodes; ++i) {
-    fscanf(fin, "%d", &cur_node);
-    //printf("from tree.cc: cur_node=%d , ", cur_node);
-    fscanf(fin, "%d", &status_int);
-
-    cur.status = (NodeStatusType)status_int;
-    assert(cur.status != EMPTY);
-
-    if (status_int == 3) {
-      //printf("before read in SPLIT\n");
-      fscanf(fin, "%hd %hd %hd %f\n", &cur.left, &cur.right, &cur.attr, &cur.split_point);
-      //printf("after read in SPLIT\n");
-     // printf("%d %d %d %f\n", left, right, attr, split_point);
-	  }
-	  else if (status_int == 2) {
-	      //printf("before read in TERMINAL\n");
-	      fscanf(fin, "%hd\n", &label_int);
-	      //printf("after read in TERMINAL\n");
-	      cur.label = (uchar)label_int;
-	  }
-    
-    cur_tree.nodes[cur_node] = cur;
-  }
-  return cur_tree;
-};
-
+// read random forest model from file
 void read_rf(char *filename) {
   FILE *fin = fopen(filename, "r");
 
-  fscanf(fin, "%d %d\n", &num_tree, &num_feature);
-  //printf("read random forest: num_tree=%d, num_feature=%d\n", num_tree, num_feature);
+  fscanf(fin, "%hd %hd\n", &num_tree, &num_feature);
+  printf("read random forest: num_tree=%d, num_feature=%d\n", num_tree, num_feature);
 
   for (i = 0; i < num_tree; ++i) {
-    forest[i] = read_tree(fin);
-    //printf("%f\n", forest[i].nodes[1].split_point);
+    fscanf(fin, "%s %hd\n", spacer, &forest[i].num_nodes);
+    if (DEBUG) {
+      printf("tree#%d, num_nodes=%d\n", i, forest[i].num_nodes);
+    }
+    for (j = 0; j < forest[i].num_nodes; ++j) {
+      fscanf(fin, "%hd", &cur_node);
+      fscanf(fin, "%hd", &status_int);
+      if (DEBUG) {
+        printf("cur_node=%d, status_int=%d\n", cur_node, status_int);
+      }
+      forest[i].nodes[j].status = (NodeStatusType)status_int;
+      assert(forest[i].nodes[j].status != EMPTY);
+      if (forest[i].nodes[j].status == 3) {
+        fscanf(fin, "%hd %hd %hd %f\n", &forest[i].nodes[j].left, &forest[i].nodes[j].right, &forest[i].nodes[j].attr, &forest[i].nodes[j].split_point);
+      }
+      else if (forest[i].nodes[j].status == 2) {
+          fscanf(fin, "%hd\n", &label_int);
+          forest[i].nodes[j].label = (uchar)label_int;
+      }
+    }    
   }
   fclose(fin);
 }
 
 static float attributes[NUM_FEATURE];
 static char result = 0;
-static short label = 0, predict_label = 0, true_label = 0;
-static short total_sample;
+static uint16 label = 0, predict_label = 0, true_label = 0;
+static uint16 total_sample;
 static tree_node n;
-int k, j;
-static short neg_vote = 0, pos_vote = 0;
-static short count = 0;
+static uint16 neg_vote = 0, pos_vote = 0;
+static uint16 count = 0;
 
+// predict the samples from a file
 void predict(char *featurefile, char *labelfile) {
   FILE *ffec = fopen(featurefile, "r");
   FILE *flabel = fopen(labelfile, "r");
-  fscanf(ffec, "%d\n", &total_sample);
+  fscanf(ffec, "%hd\n", &total_sample);
 
   for (k = 0; k < total_sample; k++) {
     for (j = 0; j < NUM_FEATURE; j++) {
       fscanf(ffec, "%f", &attributes[j]);
+      if (DEBUG) {
+        printf("%f ", attributes[j]);
+      }
     }
-    fscanf(flabel, "%d", &true_label);
+    fscanf(flabel, "%hd", &true_label);
+    if (DEBUG) {
+      printf("\nlabel=%hd\n", true_label);
+    }
+    
 
     for (i = 0; i < num_tree; i++) {
       result = 0;
       cur_node = 0;
       label = 0;
 
-      while (result != 0) {
+      while (result == 0) {
         n = forest[i].nodes[cur_node];
+        if (DEBUG) {
+          printf("status=%d\n", n.status);
+        }
         assert(n.status == TERMINAL || n.status == SPLIT);
+
+        // if this is a leaf node
         if (n.status == TERMINAL) {
-          printf("terminal\n");
+          if (DEBUG) {
+            printf("terminal label=%d\n", n.label);
+          }
           result = 1;
           label = n.label;
-        } else {
-	  printf("split: cur_node=%d, n.attr=%d, attr=%f, split=%f\n", cur_node, n.attr, attributes[n.attr], n.split_point);
+        } 
+        // if this is a non-leaf node, should split over a feature
+        else {
+          if (DEBUG) {
+            printf("split: cur_node=%d, n.attr=%d, attr=%f, split=%f\n", cur_node, n.attr, attributes[n.attr], n.split_point);
+          }
           if (attributes[n.attr] < n.split_point) {          
             cur_node = n.left;
           } else {
@@ -127,6 +132,7 @@ void predict(char *featurefile, char *labelfile) {
           }
         }
       }
+      // add a vote for this label
       if (label == 1) {
         pos_vote++;
       }
@@ -134,12 +140,15 @@ void predict(char *featurefile, char *labelfile) {
         neg_vote++;
       }
     }
+    // generate the predicted label using the majority vote
     if (pos_vote > neg_vote) {
       predict_label = 1;
     }
     else {
       predict_label = -1;
     }
+    pos_vote = 0;
+    neg_vote = 0;
     if (true_label == predict_label) {
       count++;
     }
